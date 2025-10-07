819b2815886cd56b3e270c3e632938c2cc987a04
bitmotte <bitmotte@proton.me>
2025-10-07 12:16:43 -0400

    initial commit

---
+++ MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ULTRAReplay
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "bitmotte.ULTRAReplay";
+        public const string PLUGIN_NAME = "ULTRAReplay";
+        public const string PLUGIN_VERSION = "0.0.0";
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+namespace ULTRAReplay;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
